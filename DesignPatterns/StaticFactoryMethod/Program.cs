049fa8266d17f48aa14be8b925cf0577367381cd
TakeshiYoshikawa <takeshiy096@gmail.com>
2022-05-26 18:33:03 -0300

    Add StaticFactoryMethod

---
+++ DesignPatterns/StaticFactoryMethod/Program.cs
@@ -0,0 +1,7 @@
+﻿using StaticFactoryMethod;
+
+RandomIntGenerator rig = RandomIntGenerator.LowerThan(100);
+RandomIntGenerator rig2 = RandomIntGenerator.Between(30, 40);
+
+Console.WriteLine(rig.min);
+Console.WriteLine(rig2.min);
---
+++ DesignPatterns/StaticFactoryMethod/RandomIntGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticFactoryMethod
+{
+    public class RandomIntGenerator
+    {
+        private Random random;
+
+        public int min;
+        public int max;
+
+        private RandomIntGenerator(int min, int max)
+        {
+            random = new Random();
+
+            this.min = random.Next(min, max);
+            this.max = random.Next(min, max);
+        }
+
+        public static RandomIntGenerator LowerThan(int max)
+        {
+            return new RandomIntGenerator(1, max);
+        }
+
+        public static RandomIntGenerator Between(int min, int max)
+        {
+            return new RandomIntGenerator(min, max);
+        }
+
+        public static RandomIntGenerator HigherThan(int min)
+        {
+            return new RandomIntGenerator(min, int.MaxValue);
+        }
+    }
+}
